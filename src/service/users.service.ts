f65f9ff8fd652d845cdc24eca74f82cc8b6994a1
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2021-07-22 09:33:22 +0200

    fix "createUser" (transfer of logic to the service)

--- src/service/users.service.ts
+++ src/service/users.service.ts
@@ -1,9 +1,14 @@
 import UsersDao from "../daos/users.dao";
 import { IUser } from "../model/userModel";
+import bcrypt from "bcryptjs";
 
 class UsersService {
   async create(resource: IUser) {
-    return UsersDao.createUser(resource);
+    const newUser: IUser = resource;
+    const newPassword = await bcrypt.hash(resource.password!, 10);
+    newUser.password = newPassword;
+
+    return UsersDao.createUser(newUser);
   }
 
   async authenticate(login: string, password: string) {
