af8f70956fa8d5419f7dc8db4b3b5094130e200b
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2021-07-26 13:52:16 +0200

    user validation errors with information

--- src/validators/users.validator.ts
+++ src/validators/users.validator.ts
@@ -1,5 +1,48 @@
 import { body, ValidationChain } from "express-validator";
 
-const validateFirstName = body("first_name").isString().notEmpty();
+const messageString = "Field must be a string!";
+const messageEmpty = "Field can not be empty!";
 
-export const validateUserRegister: ValidationChain[] = [validateFirstName];
+const validateFirstName = body("first_name")
+  .isString()
+  .withMessage(messageString)
+  .notEmpty()
+  .withMessage(messageEmpty);
+const validateFirstLast = body("last_name")
+  .isString()
+  .withMessage(messageString)
+  .notEmpty()
+  .withMessage(messageEmpty);
+const validateLogin = body("login")
+  .isString()
+  .withMessage(messageString)
+  .notEmpty()
+  .withMessage(messageEmpty);
+const validateEmail = body("email")
+  .isString()
+  .withMessage(messageString)
+  .notEmpty()
+  .withMessage(messageEmpty)
+  .isEmail()
+  .withMessage("This is not an email!");
+const validatePassword = body("password")
+  .isString()
+  .withMessage(messageString)
+  .notEmpty()
+  .withMessage(messageEmpty)
+  .isStrongPassword()
+  .withMessage("Password is too weak!");
+const validateRole = body("role")
+  .isString()
+  .withMessage(messageString)
+  .notEmpty()
+  .withMessage(messageEmpty);
+
+export const validateUserRegister: ValidationChain[] = [
+  validateFirstName,
+  validateFirstLast,
+  validateLogin,
+  validateEmail,
+  validatePassword,
+  validateRole,
+];
