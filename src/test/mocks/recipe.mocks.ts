fa86c8a7210f0b95b90271830b83bb9531b70091
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2021-07-29 14:05:14 +0200

    add next test for recipe

--- src/test/mocks/recipe.mocks.ts
+++ src/test/mocks/recipe.mocks.ts
@@ -1,5 +1,4 @@
 import { faker } from '../test.config';
-
 import { Recipe } from '../../model/recipeModel';
 
 const createRecipePayload = {
@@ -9,16 +8,24 @@ const createRecipePayload = {
   recipe: faker.random.words(),
 };
 
-// export const createRecipePayload = {
-//     name: 'gfd', //faker.name.title(),
-//     type: 'gfd', //faker.name.findName(),
-//     photo:
-//       'https://www.kwestiasmaku.com/sites/v123.kwestiasmaku.com/files/styles/zdjecie_glowne/public/gumbo.jpg?itok=2-d_W8VD', //faker.internet.url(),
-//     recipe: 'gh', //faker.random.words(),
-//   };
+const createRecipePayloadTest = {
+  name: faker.name.title(),
+  type: faker.name.findName(),
+  photo: faker.internet.url(),
+  recipe: faker.random.words(),
+  added_by: '',
+};
+
+const createRecipeTest = async function (id: string) {
+  createRecipePayloadTest.added_by = id;
+  const recipeToSave = new Recipe(createRecipePayloadTest);
+  await recipeToSave.save();
+
+  return recipeToSave.id;
+};
 
 const deleteAllRecipes = function () {
   return Recipe.deleteMany();
 };
 
-export { createRecipePayload, deleteAllRecipes };
+export { createRecipePayload, createRecipeTest, deleteAllRecipes };
--- src/test/spec/recipe.spec.ts
+++ src/test/spec/recipe.spec.ts
@@ -1,14 +1,17 @@
 import { chai, expect, app } from '../test.config';
-import { createRecipePayload, deleteAllRecipes } from '../mocks/recipe.mocks';
+import { createRecipePayload, createRecipeTest, deleteAllRecipes } from '../mocks/recipe.mocks';
 
 import { generateToken, deleteAllUsers } from '../mocks/user.mocks';
 
 let token = '';
+let id = '';
 
 beforeEach('Add new user and return token', async function () {
+  await deleteAllRecipes();
   await deleteAllUsers();
   const data = await generateToken();
-
+  const _id = data._id;
+  id = await createRecipeTest(_id);
   token = `Bearer ${data.Token}`;
 });
 
@@ -21,7 +24,32 @@ describe('Recipe', function () {
   describe('POST /recipes/', function () {
     it('should create recipe', async function () {
       const response = await chai.request(app).post('/recipes').set('Authorization', token).send(createRecipePayload);
+
       expect(response).to.have.status(201);
+      expect(response.error).to.be.false;
+      expect(response.body).to.not.be.null;
+    });
+  });
+
+  describe('GET /recipes/', function () {
+    it('should get recipes', async function () {
+      const response = await chai.request(app).get('/recipes').set('Authorization', token).send(createRecipePayload);
+
+      expect(response).to.have.status(200);
+      expect(response.error).to.be.false;
+      expect(response.body).to.not.be.null;
+    });
+  });
+
+  describe('GET /recipes/', function () {
+    it('should get recipe by id', async function () {
+      const response = await chai
+        .request(app)
+        .get('/recipes/' + id)
+        .set('Authorization', token)
+        .send(createRecipePayload);
+
+      expect(response).to.have.status(200);
       expect(response.error).to.be.false;
       expect(response.body).to.not.be.null;
     });
