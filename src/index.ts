50f03e5d4a5f96538a0f3c77092c0c19389b918e
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2021-07-29 15:48:07 +0200

    First step

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import cors from 'cors';
 import errorMiddleware from './middleware/error.middleware';
 import morgan from 'morgan';
 
+import { lodash } from 'lodash';
+
 dotenv.config();
 
 import * as db from './mongodb/db';
@@ -38,5 +40,46 @@ db.init();
 app.listen(port, function () {
   console.log('Running REST_RECIPES on port ' + port);
 });
+
+const myObjectA = {
+  someFieldA: 'some value',
+  someFieldB: 1234,
+  someFieldC: {
+    subFieldA: 'some value',
+    subFieldB: 1234,
+  },
+  someFieldD: ['some value', 'some value 2'],
+  someFieldE: {
+    subFieldA: {
+      subSubFieldA: 'some value',
+      subSubFieldB: 1234,
+    },
+    someFieldB: ['some value', 'some value 2'],
+  },
+};
+console.log('\nA ', myObjectA);
+
+// console.log('\ntesta', myObjectA.someFieldA);
+// console.log('\ntestb', myObjectA.someFieldB);
+
+// console.log('\ntestc', myObjectA.someFieldC);
+// console.log('\ntestd', myObjectA.someFieldD);
+
+// console.log('\nteste', myObjectA.someFieldE);
+
+const myObjectB = myObjectA;
+
+console.log('\nB ', myObjectB);
+
+myObjectB.someFieldA = 'some value 2';
+
+console.log('\nB ', myObjectB);
+
+console.log('test', myObjectA);
+
+const objects = [{ a: 1 }, { b: 2 }];
+
+const deep = _.cloneDeep(objects);
+console.log(deep[0] === objects[0]);
 
 export default app;
