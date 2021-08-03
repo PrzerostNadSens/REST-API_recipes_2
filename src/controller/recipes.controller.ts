384a068f4e6d4e53e40336a4d5f75d6903ad3c18
PrzerostNadSens <59373916+PrzerostNadSens@users.noreply.github.com>
2021-08-03 15:37:51 +0200

    Add pagination for "getAllRecipe"

--- src/controller/recipes.controller.ts
+++ src/controller/recipes.controller.ts
@@ -1,4 +1,4 @@
-import { IRecipe, Recipe, OmitIRecipe } from '../model/recipe.model';
+import { IRecipe, Recipe, OmitIRecipe, RecipeDocument } from '../model/recipe.model';
 import { Request, Response } from 'express';
 import { returnId, AuthorizedRequest } from '../mongodb/authorize';
 import RecipesService from '../service/recipes.service';
@@ -39,13 +39,13 @@ class RecipesController {
       const {
         query: { currentPage, pageSize },
       } = req;
-      const count = await Recipe.estimatedDocumentCount();
+      const totalElements = await Recipe.estimatedDocumentCount();
       const { limit, offset } = calculateLimitAndOffset(currentPage, pageSize);
       const filter: OmitIRecipe = req.query;
-      const rows = await RecipesService.getAll(filter, limit, offset);
-      const meta = paginate(currentPage, count, rows, pageSize);
+      const recipes = await RecipesService.getAll(filter, limit, offset);
+      const meta = paginateInfo(currentPage, totalElements, recipes, pageSize);
 
-      return res.send({ rows, meta });
+      return res.send({ recipes, meta });
     } catch (e) {
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(internalServerError);
     }
@@ -111,3 +111,17 @@ class RecipesController {
   }
 }
 export default new RecipesController();
+
+function paginateInfo(page: any, totalElements: number, recipes: RecipeDocument[], size: any) {
+  const { currentPage, pageCount, pageSize, count } = paginate(page, totalElements, recipes, size);
+  let firstPage = false;
+  let lastPage = false;
+  if (currentPage == '1') {
+    firstPage = true;
+  }
+  if (currentPage == pageCount) {
+    lastPage = true;
+  }
+  const info = { currentPage, pageCount, pageSize, count, firstPage, lastPage };
+  return info;
+}
